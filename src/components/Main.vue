030eca516361eeb62fbfc9c7ecf5843b8a4413ea
RodrigoVitor <rodrigovitorsc2001@gmail.com>
2021-06-02 11:59:08 -0300

    adicionando navbar

---
+++ src/components/Main.vue
@@ -0,0 +1,61 @@
+<template>
+    <div>
+        <nav>
+            <h2>crowdfunding</h2>
+            <div>
+                <ul>
+                    <li><a href="#">About</a></li>
+                    <li><a href="#">Discover</a></li>
+                    <li><a href="#">Get started</a></li>
+                </ul>
+            </div>
+            <Menu />
+        </nav>
+    </div>
+</template>
+
+<script>
+import Menu from '../components/Menu'
+export default {
+    components: {
+        Menu
+    }
+}
+</script>
+
+<style scoped>
+/* reset */
+ul {list-style: none;}
+a{text-decoration: none; color: rgba(255, 255, 255, 0.822)}
+/* -------------------- */
+nav {
+    display:flex;
+    justify-content: space-between;
+    background-image:url('../assets/image-hero-desktop.jpg');
+    height:400px
+}
+nav h2, ul {
+    color:white;
+    margin-top:40px
+}
+nav h2 {margin-left:175px}
+nav ul{margin-right:85px}
+nav ul li {
+    display:inline-block;
+    margin-left:15px
+}
+
+@media screen and (max-width: 720px) {
+    nav {
+        background-image:url('../assets/image-hero-mobile.jpg');
+        height:600px
+    }
+    nav ul {
+        display:none
+    }
+    nav h2 {margin-left:55px;font-size: 2rem;}
+}
+@media screen and (max-width: 375px) {
+    nav {display:none}
+}
+</style>
---
+++ src/components/Menu.vue
@@ -0,0 +1,33 @@
+<template>
+    <div>
+        <nav>
+            <img src="../assets/icon-hamburger.svg" alt="icon-hamburger">
+            <ul>
+                <li><a href="#">About</a></li>
+                 <li><a href="#">Discover</a></li>
+                 <li><a href="#">Get started</a></li>
+            </ul>
+        </nav>
+    </div>
+</template>
+
+<style scoped>
+/* reset */
+ul {list-style: none;}
+a{text-decoration: none; color: rgba(255, 255, 255, 0.822)}
+/* ----------- */
+div {
+    position: absolute; 
+    right:40px;
+    top:40px;}
+nav img {
+    cursor: pointer;
+}
+@media screen and (min-width: 720px) {
+    nav {display:none}
+}
+@media screen and (max-width: 720px) {
+    nav ul {display:none}
+}
+
+</style>
