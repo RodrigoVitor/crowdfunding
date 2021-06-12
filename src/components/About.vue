fe7ef829cb5a53f924af4c6b1ada569bf2f011bd
RodrigoVitor <rodrigovitorsc2001@gmail.com>
2021-06-12 19:02:21 -0300

    finalizando front-end

--- src/components/About.vue
+++ src/components/About.vue
@@ -13,11 +13,36 @@
                     <button>Select Reward</button>
                 </div>
             </div>
+
+            <div class="component-blackEdition">
+                <h2>Black Edition Stand  <small>Pledge $75 or more</small></h2>
+                <p>You get a Black Special Edition computer stand and a personal thank you. You'll be added to our Backer member list. Shipping is included</p>
+                <div class="main">
+                    <h1>64 <small>left</small></h1>
+                    <button>Select Reward</button>
+                </div>
+            </div>
+
+            <div class="component-special">
+                <h2>Mahogany Special Edition  <small>Pledge $200 or more</small></h2>
+                <p>You get two Special Edition Mahogany stands, o Backer T-shirt, and a personal thank you, You-ll be added to our Backer member list. Shipping is included.</p>
+                <div class="main">
+                    <h1>0 <small>left</small></h1>
+                    <button>Select Reward</button>
+                </div>
+            </div>
         </div>
     </div>
 </template>
 
 <style scoped>
+/* Global */
+.main {
+    display:flex;
+    justify-content: space-between;
+    align-items: center;
+}
+/* --------- */
 .container {
     width:50%;
     margin: 20px auto auto auto;
@@ -45,37 +70,33 @@
     padding:20px;
     border-radius:10px
 }
-.component-bamboo > h2 {
+.component-bamboo > h2, .component-blackEdition > h2 {
     font-size:1.2rem;
     display:flex;
     justify-content: space-between;
 }
-.component-bamboo h2 > small {
+.component-bamboo h2 > small, .component-blackEdition h2 > small {
     font-size:0.7rem;
     color:hsl(176, 72%, 28%)
 }
-.component-bamboo > p {
+.component-bamboo > p, .component-blackEdition > p {
     text-align: justify;
     font-size:1rem;
     margin-top:20px;
     color:gray;
     line-height:30px
 }
-.component-bamboo h1 {
+.component-bamboo h1, .component-blackEdition h1 {
     display:flex;
     align-items: center;
     margin-top:25px
 }
-.component-bamboo h1 > small {
+.component-bamboo h1 > small, .component-blackEdition h1 > small {
     font-size:.8rem;
     color:gray
 }
-.main {
-    display:flex;
-    justify-content: space-between;
-    align-items: center;
-}
-.component-bamboo button {
+
+.component-bamboo button ,.component-blackEdition button {
     width:150px;
     height:50px;
     background-color: hsl(176, 50%, 47%);
@@ -92,14 +113,14 @@
         text-align:left
     }
     /* component-bamboo */
-    .component-bamboo {
+    .component-bamboo, .component-blackEdition {
         width:60%;
     }
-    .component-bamboo > h2 {
+    .component-bamboo > h2, .component-blackEdition > h2 {
         display:flex;
         flex-direction: column;
     }
-    .component-bamboo > p {
+    .component-bamboo > p, .component-blackEdition > p {
         text-align: left;
         line-height:20px
     }
@@ -107,5 +128,57 @@
         display:flex;
         flex-direction: column;
     }
+    .component-blackEdition button, .component-bamboo button {
+        width:140px;
+        height:40px;
+    }
+}
+
+/* component-blackEdition */
+.component-blackEdition{
+    border:1px solid #ccc;
+    width:80%;
+    margin: 25px auto auto auto;
+    padding:20px;
+    border-radius:10px
+}
+
+/* component-blackEdition */
+.component-special{
+    border:1px solid #ccc;
+    width:80%;
+    margin: 25px auto auto auto;
+    padding:20px;
+    border-radius:10px
+}
+.component-special > h2 {
+    font-size:1.2rem;
+    display:flex;
+    justify-content: space-between;
+    color:gray;
+    font-weight:bold
+}
+.component-special h2 > small {
+    font-size:0.7rem;
+    color:hsl(176, 72%, 28%)
+}
+.component-special > p {
+    text-align: justify;
+    font-size:1rem;
+    margin-top:20px;
+    color:rgb(202, 194, 194);
+    line-height:30px;
+}
+.component-special button {
+   width:150px;
+    height:50px;
+    background-color: gray;
+    color:white;
+    border:none;
+    border-radius:25px;
+    cursor:pointer;
+}
+.component-special div > h1 {
+    color:gray
 }
 </style>
